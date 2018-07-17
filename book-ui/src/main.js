8f00b4fd47746f57466725a009705aebd224aacd
yohanswanepoel <johannes.swanepoel@gmail.com>
2018-07-17 13:20:22 +1000

    Update main.js
    Removed keycloak dependencies - should create a single branch with a switch

--- book-ui/src/main.js
+++ book-ui/src/main.js
@@ -1,6 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import VueKeyCloak from '@dsb-norge/vue-keycloak-js';
 import 'bootstrap/dist/css/bootstrap.css';
 import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
@@ -12,47 +11,16 @@ Vue.config.productionTip = false;
 const url = window.location.hostname;
 
 const domainURL = url.substring(url.indexOf('.'), url.length);
-const kpath = `http://keycloak-myproject${domainURL}/auth`;
-
-const kc = {
-  authRealm: 'bookapi',
-  authUrl: kpath,
-  authClientId: 'vue',
-  authRedirectUri: url,
-};
-
-const kcInit = {
-  onLoad: 'login-required',
-};
 
 Vue.use(BootstrapVue);
 
-Vue.use(VueKeyCloak, {
-  config: kc,
-  init: kcInit,
-  onReady: (keycloak) => {
-    console.log(`I wonder what Keycloak returns: ${keycloak}`);
-    /* eslint-disable no-new */
-    new Vue({
-      el: '#app',
-      router,
-      template: '<App/>',
-      render: h => h(App),
-      data: {
-        baseURL: 'http://python-app-myproject.192.168.42.125.nip.io',
-      },
-    });
-  },
-});
-
-
 /* eslint-disable no-new */
-/* new Vue({
+new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>',
   data: {
-    baseURL: 'http://python-app-myproject.192.168.42.125.nip.io',
+    baseURL: domainURL,
   },
-}); */
+}); 
